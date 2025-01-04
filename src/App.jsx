905213a62556181bb5ae9ff49dcfa86e37588315
biswadeep <biswadeepmazumder29@gmail.com>
2025-01-04 13:39:07 +0530

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
         <div className="h-screen flex justify-center items-center bg-gradient-to-r from-purple-500 to-pink-500">
             <div className="text-center">
                 <h1 className="text-5xl font-bold text-white mb-4 ">
-                    Welcome to Our App!
+                    Welcome to out ext!
                 </h1>
                 <p className="text-2xl text-white opacity-80 mb-8">
                     We're thrilled to have you on board!
